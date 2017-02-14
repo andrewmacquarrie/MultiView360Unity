7283f2babc14c39e56227d29816385ffe7736b3d
andrewmacquarrie <andrew.macquarrie@gmail.com>
2017-02-14 13:01:21 +0000

    adding files

---
+++ Assets/PopMovieSwitch.cs
@@ -0,0 +1,143 @@
+using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+///		PopMovieSimple is a very simple example of how to use a PopMovie instance to
+///		play a movie to a specified texture using games timedelta to control the playback.
+/// </summary>
+[AddComponentMenu("PopMovie/PopMovieSwitch")]
+public class PopMovieSwitch : MonoBehaviour {
+
+    public Texture TargetTexture;
+    public string[] Filenames;
+
+	[Header("Current time in the movie in seconds")]
+	[Range(0,10)]
+	public float			MovieTime = 0;
+
+	[Header("Scalar to make the movie playback faster or slower")]
+	[Range(0,3)]
+	public float			MovieTimeScalar = 1;
+
+	public PopMovieParams	Parameters;
+    public PopMovie[] Movies = new PopMovie[5];
+
+    public int              PlayingMovieIndex = 0;
+
+	public bool				PlayOnAwake = true;
+	public bool				Playing = false;
+
+	public UnityEngine.Events.UnityEvent	OnFinished;
+
+
+	[Tooltip("Which audio stream to use")]
+	public int				AudioStream = 0;
+
+	[Tooltip("Enable debug logging when movie starts. Same as the global option, but automatically turns it on")]
+	public bool				EnableDebugLog = false;
+
+	void Awake() {
+		if (PlayOnAwake ) {
+			Play ();
+		}
+	}
+
+	public void Play()
+	{
+        for (int i = 0; i < Filenames.Length; i++)
+        {
+            if (Movies[i] == null)
+            {
+                Movies[i] = new PopMovie(Filenames[i], Parameters, MovieTime);
+            }
+        }
+        /*
+        if (Movies[PlayingMovieIndex] == null)
+        {
+            Movies[PlayingMovieIndex] = new PopMovie(Filenames[PlayingMovieIndex], Parameters, MovieTime);
+			if ( EnableDebugLog )
+				PopMovie.EnableDebugLog = true;
+		}*/
+
+		Playing = true;
+	}
+
+	public void Pause()
+	{
+		Playing = false;
+	}
+
+	public void Stop()
+	{
+		Playing = false;
+
+        for (int i = 0; i < Filenames.Length; i++)
+        {
+            if (Movies[i] != null)
+            {
+                Movies[i].Free();
+                Movies[i] = null;
+            }
+        }
+	}
+
+	public void UpdateTextures()
+	{
+        if (Movies[PlayingMovieIndex] != null && TargetTexture != null)
+            Movies[PlayingMovieIndex].UpdateTexture(TargetTexture);
+	}
+
+	public void Update () {
+
+		if (Playing) {
+			MovieTime += Time.deltaTime * MovieTimeScalar;
+
+            /*
+            if (Movies[PlayingMovieIndex] != null)
+                Movies[PlayingMovieIndex].SetTime(MovieTime);
+                */
+            
+            UpdateTextures();
+
+			//	detect finish
+            if (Movies[PlayingMovieIndex] != null)
+			{
+                var Duration = Movies[PlayingMovieIndex].GetDuration();
+				//	if duration is zero, we don't know it yet (could be streaming, video camera, or may not have loaded meta information yet)
+				if ( Duration > 0 && MovieTime >= Duration )
+				{
+					OnFinished.Invoke();
+					Playing = false;
+				}
+			}
+		}
+
+	}
+
+    /*
+	//	this gets automatically called if this object has an AudioSource component
+	void OnAudioFilterRead(float[] data,int Channels)
+	{
+        if (Movies[PlayingMovieIndex] == null)
+			return;
+
+        uint StartTime = Movies[PlayingMovieIndex].GetTimeMs();
+
+        Movies[PlayingMovieIndex].GetAudioBuffer(data, Channels, StartTime, AudioStream);
+	}*/
+
+    public void SwitchToSphere(int i)
+    {
+        Playing = false;
+        PlayingMovieIndex = i;
+        Movies[PlayingMovieIndex].SetTime(MovieTime);
+        Playing = true;
+        
+        /*
+        if (Movies[PlayingMovieIndex] == null)
+        {
+            Movies[PlayingMovieIndex] = new PopMovie(Filenames[PlayingMovieIndex], Parameters, MovieTime);
+        }*/
+    }
+}
---
+++ Assets/SphereSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSwitch : MonoBehaviour {
+
+    public PopMovieSwitch popMovieSwitch;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            popMovieSwitch.SwitchToSphere(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            popMovieSwitch.SwitchToSphere(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            popMovieSwitch.SwitchToSphere(2);
+        }
+    }
+}
